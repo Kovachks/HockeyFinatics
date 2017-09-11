0aa04293c487b83662aa394905b0d2f69781813c
Keith Kovach <kovachks90@gmail.com>
2017-09-11 13:13:20 -0400

    updated css

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,6 @@
 var path = require("path");
 
-var express = require("express")
+var express = require("express");
 
 var router = express.Router();
 
@@ -8,16 +8,16 @@ module.exports = function(app) {
 	app.get("/", function(req, res) {
 		res.sendFile(path.join(__dirname, "../public/index.html"));
 	})
-}
+};
 
 router.get("/", function(req, res) {
 	res.sendFile(path.join(__dirname, "../public/index.html"))
-})
+});
 
 router.get("/:team", function(req, res) {
 	var team = req.params.team;
 	console.log("test: " + team)
 	res.render(team)
-})
+});
 
 module.exports = router;
