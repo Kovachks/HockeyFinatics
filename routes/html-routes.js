b6685613f5e70cba8c5a971d3c721a9f3328b0ce
Johcare <johcare2@gmail.com>
2017-09-12 14:03:58 -0400

    new routes,css added

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,17 +7,35 @@ var router = express.Router();
 module.exports = function(app) {
 	app.get("/", function(req, res) {
 		res.sendFile(path.join(__dirname, "../public/index.html"));
-	})
+	});
+	// LOG IN HTML ROUTE
+	app.get("/login", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+	});
+	// REGISTER HTML ROUTE
+		app.get("/registration", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/register.html"));
+	});
+	// FORGOT PASSWORD ROUTE
+		app.get("/forgot-password", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/forgot-password.html"));
+		});
+			app.get("/contact", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/contact.html"));
+      });
 };
 
-router.get("/", function(req, res) {
-	res.sendFile(path.join(__dirname, "../public/index.html"))
-});
+// router.get("/", function(req, res) {
+// 	res.sendFile(path.join(__dirname, "../public/index.html"))
+// });
 
 router.get("/:team", function(req, res) {
 	var team = req.params.team;
 	console.log("test: " + team)
 	res.render(team)
 });
+// LOGIN PAGE
+
+
 
 module.exports = router;
