b440b8c92bbfda154f6f04a9bde64d8ef849ba05
Keith Kovach <kovachks90@gmail.com>
2017-09-11 22:15:20 -0400

    updated team handlebars

---
+++ db/seeds.sql
@@ -0,0 +1,93 @@
+use hockeystats_db;
+
+select * from teaminfo;
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("avalanche", "#236192", "#6f263d");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("ducks", "#85714d", "#fc4c02");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("coyotes", "#862633", "#ddcba4");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("bruins", "#ffb81c", "#010101");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("sabres", "#041e42", "#ffb81c");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("flames", "#c8102e", "#f1be48");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("hurricanes", "#c8102e", "#010101");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("blackhawks", "#c8102e", "#010101");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("bluejackets", "#041e42", "#c8102e");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("stars", "#006341", "#8a8d8f");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("redwings", "#c8102e", "#f2f5f9");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("oilers", "#00205b", "#cf4520");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("panthers", "#041e42", "#b9957b");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("kings", "#010101", "#a2aaad");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("wild", "#154734", "#ddcba4");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("canadiens", "#001e62", "#a6192e");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("predators", "#041e42", "#ffb81c");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("devils", "#c8102e", "#010101");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("islanders", "#003087", "#fcfc02");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("rangers", "#0033a0", "#c8102e");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("senators", "#c8102e", "#c69214");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("flyers", "#010101", "#fa4616");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("penguins", "#ffb81c", "#010101");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("sharks", "#006272", "#e57200");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("blues", "#003087", "#ffb81c");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("lightning", "#00205b", "#f2f5f9");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("canucks", "#00205b", "#97999b");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("goldenknights", "#010101", "#b4957a");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("capitals", "#041e42", "#a6192e");
+
+insert into teaminfo (teamName, primaryColor, secondaryColor)
+values("jets", "#041e42", "#c8102e");
