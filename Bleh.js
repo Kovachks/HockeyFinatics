100a0f0bf3a87b86945dd02cd3b910f027bd0f40
Tiara Hamilton <tnhami63@gmail.com>
2017-09-14 14:10:43 -0400

    pullng

--- Bleh.js
+++
@@ -1,9 +0,0 @@
-//Simple algorithm
-
-//wins/total #games (*100) = % <-- equation
-//Display the percentages -- Append the percentage score to the table via col.
-//run the math
-//then find someway to append it to the table
-
-
-//We have a template (team.handlebars) but how do you replace different information
