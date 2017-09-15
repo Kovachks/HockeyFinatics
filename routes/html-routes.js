500a6375049ce3bc1154cc090342e5d664d5895b
Keith Kovach <kovachks90@gmail.com>
2017-09-15 00:50:47 -0400

    stuck.  need a workaround

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -41,19 +41,19 @@ module.exports = function(app) {
 
 router.get("/:team", function(req, res) {
 	var team = req.params.team;
-	// db.playerStats.findAll({
-	// 	where: {
-	// 		mascot: team
-	// 	},
-	// 	include: [
-	// 	{
-	// 		model: db.teaminfo,
-	// 	}
-	// 	,{
-	// 		model: db.gameSchedule,
-	// 	}]
-	sequelize.query("SELECT `teaminfo`.*, `playerStats`.`id` AS `playerStats.id`, `playerStats`.`name` AS `playerStats.name`, `playerStats`.`age` AS `playerStats.age`, `playerStats`.`nationality` AS `playerStats.nationality`, `playerStats`.`team` AS `playerStats.team`, `playerStats`.`mascot` AS `playerStats.mascot`, `playerStats`.`position` AS `playerStats.position`, `playerStats`.`games_played` AS `playerStats.games_played`, `playerStats`.`goals` AS `playerStats.goals`, `playerStats`.`assists` AS `playerStats.assists`, `playerStats`.`points` AS `playerStats.points`, `gameSchedules`.`id` AS `gameSchedules.id`, `gameSchedules`.`vs` AS `gameSchedules.vs`, `gameSchedules`.`opponent` AS `gameSchedules.opponent`, `gameSchedules`.`mascot` AS `gameSchedules.mascot`, `gameSchedules`.`date` AS `gameSchedules.date`, `gameSchedules`.`time` AS `gameSchedules.time`, `gameSchedules`.`timezone` AS `gameSchedules.timezone`, `gameSchedules`.`chanceToWin` AS `gameSchedules.chanceToWin` FROM (SELECT `teaminfo`.`id`, `teaminfo`.`mascot`, `teaminfo`.`teamName`, `teaminfo`.`primaryColor`, `teaminfo`.`secondaryColor` FROM `teaminfos` AS `teaminfo` WHERE `teaminfo`.`mascot` = 'bluejackets' LIMIT 1) AS `teaminfo` LEFT OUTER JOIN `playerStats` AS `playerStats` ON `teaminfo`.`mascot` = `playerStats`.`mascot` LEFT OUTER JOIN `gameSchedules` AS `gameSchedules` ON `teaminfo`.`mascot` = `gameSchedules`.`mascot`;"
-	).then(function(result) {
+	db.playerStats.findAll({
+		where: {
+			mascot: team
+		},
+		include: [
+		{
+			model: db.teaminfo,
+		}
+		,{
+			model: db.gameSchedule,
+		}]
+	// sequelize.query("SELECT `teaminfo`.*, `playerStats`.`id` AS `playerStats.id`, `playerStats`.`name` AS `playerStats.name`, `playerStats`.`age` AS `playerStats.age`, `playerStats`.`nationality` AS `playerStats.nationality`, `playerStats`.`team` AS `playerStats.team`, `playerStats`.`mascot` AS `playerStats.mascot`, `playerStats`.`position` AS `playerStats.position`, `playerStats`.`games_played` AS `playerStats.games_played`, `playerStats`.`goals` AS `playerStats.goals`, `playerStats`.`assists` AS `playerStats.assists`, `playerStats`.`points` AS `playerStats.points`, `gameSchedules`.`id` AS `gameSchedules.id`, `gameSchedules`.`vs` AS `gameSchedules.vs`, `gameSchedules`.`opponent` AS `gameSchedules.opponent`, `gameSchedules`.`mascot` AS `gameSchedules.mascot`, `gameSchedules`.`date` AS `gameSchedules.date`, `gameSchedules`.`time` AS `gameSchedules.time`, `gameSchedules`.`timezone` AS `gameSchedules.timezone`, `gameSchedules`.`chanceToWin` AS `gameSchedules.chanceToWin` FROM (SELECT `teaminfo`.`id`, `teaminfo`.`mascot`, `teaminfo`.`teamName`, `teaminfo`.`primaryColor`, `teaminfo`.`secondaryColor` FROM `teaminfos` AS `teaminfo` WHERE `teaminfo`.`mascot` = 'bluejackets' LIMIT 1) AS `teaminfo` LEFT OUTER JOIN `playerStats` AS `playerStats` ON `teaminfo`.`mascot` = `playerStats`.`mascot` LEFT OUTER JOIN `gameSchedules` AS `gameSchedules` ON `teaminfo`.`mascot` = `gameSchedules`.`mascot`;"
+	}).then(function(result) {
 	console.log("Result: " + JSON.stringify({result}))	
 	res.render("team", {team:result})
 	});
